1ad96974c5b202642297f5b5bb8e687bd1275faf
David Bauer <dmb172@case.edu>
2016-03-27 14:27:20 -0400

    Added STRIPS action for move.
    Signed-off-by: David Bauer <dmb172@case.edu>

---
+++ src/edu/cwru/sepia/agent/planner/actions/MoveStripsAction.java
@@ -0,0 +1,37 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import java.util.List;
+
+import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.Position;
+import edu.cwru.sepia.util.Direction;
+
+public class MoveStripsAction implements StripsAction {
+	
+	private final Direction direction;
+	private final int peasantId;
+	
+	public MoveStripsAction(Direction direction, int peasantId) {
+		this.direction = direction;
+		this.peasantId = peasantId;
+	}
+
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		Position peasantPosition = state.getPeasantPosition(peasantId);
+		Position nextPosition = peasantPosition.move(direction);
+		List<Position> invalidPositions = state.getOccupiedPositions();
+		if (nextPosition.inBounds(state.getXExtent(), state.getYExtent())
+				&& !invalidPositions.contains(nextPosition)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public GameState apply(GameState state) {
+		return state.applyPeasantMove(peasantId, direction);
+	}
+
+}
