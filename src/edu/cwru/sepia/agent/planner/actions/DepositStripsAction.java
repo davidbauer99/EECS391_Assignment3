fc923874f2a0ef62725cbabc3de0a37a6b3e51d2
Daedalus359 <kbradner112@gmail.com>
2016-03-27 15:59:58 -0400

    implemented DepositStrips

--- src/edu/cwru/sepia/agent/planner/actions/DepositStripsAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/DepositStripsAction.java
@@ -1,7 +1,5 @@
 package edu.cwru.sepia.agent.planner.actions;
 
-import java.util.List;
-
 import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.Position;
 import edu.cwru.sepia.util.Direction;
@@ -23,8 +21,21 @@ public class DepositStripsAction implements StripsAction
     Position peasantPosition = state.getPeasantPosition (peasantID);
     Position storagePosition = peasantPosition.move (direction);
 
-    List <Position> storagePositions = state.getOccupiedPositions ();
-    return false;
+    Position townHallPosition = state.getTownHallPosition ();
+
+    if (storagePosition.equals (townHallPosition))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  public GameState apply (GameState state)
+  {
+    return state.applyAction (this);
   }
 
 }
