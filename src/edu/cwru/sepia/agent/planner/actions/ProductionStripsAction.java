edae555d0ac2c2c64241750506791fc654011b0f
Daedalus359 <kbradner112@gmail.com>
2016-03-27 16:20:32 -0400

    ProductionStripsAction

--- src/edu/cwru/sepia/agent/planner/actions/ProductionStripsAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/ProductionStripsAction.java
@@ -1,6 +1,18 @@
 package edu.cwru.sepia.agent.planner.actions;
 
+import edu.cwru.sepia.agent.planner.GameState;
+
 public class ProductionStripsAction
 {
+  public boolean preconditionsMet (GameState state)
+  {
+    boolean enoughGold = (400 <= state.getCurrentGold ()); // need 400 //
+    boolean enoughFood = (1 <= state.getCurrentFood ()); // need 1 //use int foodAvailable
+    return (enoughGold && enoughFood);
+  }
 
+  public GameState apply (GameState state)
+  {
+    return state.applyAction (this);
+  }
 }
